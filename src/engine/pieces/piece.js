9beb46cc5a223aaea4c2ea43e4b2da7df600fb83
Luke Miles <luke.miles@softwire.com>
2019-07-08 14:00:55 +0100

    Implement rook movement

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,3 +1,5 @@
+import Square from '../square';
+
 export default class Piece {
     constructor(player) {
         this.player = player;
@@ -12,5 +14,23 @@ export default class Piece {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
         this.hasMoved = true;
+    }
+
+    getMovesInDirection(board, { row: curRow, col: curCol }, dx, dy) {
+        const moves = [];
+
+        let valid = true;
+        while (valid) {
+            curRow += dy;
+            curCol += dx;
+
+            const square = Square.at(curRow, curCol);
+
+            valid = board.isOnBoard(square) && !board.getPiece(square);
+
+            if (valid) moves.push(square);
+        }
+
+        return moves;
     }
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -6,6 +6,12 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+        const currentSquare = board.findPiece(this);
+
+        const moves = directions.flatMap(([dx, dy]) => this.getMovesInDirection(board, currentSquare, dx, dy));
+
+        return moves;
     }
 }
